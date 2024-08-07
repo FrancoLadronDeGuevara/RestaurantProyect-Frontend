ec1960206eea1522a0cc03d9d8a4ef4214ee0cd0
Franco Guevara <francoguevara93@gmail.com>
2024-08-07 19:57:43 -0300

    Added API url to clientAxios

--- src/utils/clientAxios.js
+++ src/utils/clientAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const clientAxios = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://restaurant-proyect-backend.vercel.app/api",
     headers: {
         'Content-Type': 'application/json',
     },
